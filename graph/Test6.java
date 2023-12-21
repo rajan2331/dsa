f61057450a6a80341de9918c05f206327882a817
LMN.RAJAN <rajansharma2331@gmail.com>
2023-12-21 16:14:27 +0400

    test

---
+++ graph/Test6.java
@@ -0,0 +1,69 @@
+package com.springboot.chatapp.domain;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+class Pair{
+	public String word;
+	public Integer step;
+	public Pair(String word, Integer step) {
+		super();
+		this.word = word;
+		this.step = step;
+	}
+	
+	
+}
+public class Test6 {
+
+	public static void main(String[] args) {
+		
+		String beginWord = "a", endWord = "c" ;
+		String[] wordList = {"a","b","c"};
+		
+		
+		System.out.println(ladderLength(beginWord, endWord, Arrays.asList(wordList)));
+		
+	}
+    public static int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        int count=0;
+        Set<String> set = new HashSet();
+        for(String word : wordList)
+        	set.add(word);
+        
+        Queue<Pair> queue = new LinkedList<Pair>();
+        queue.add(new Pair(beginWord,1));
+        
+        while(!queue.isEmpty())
+        {
+        	String currWord= queue.peek().word;
+        	Integer step= queue.peek().step;
+        	count=Math.max(step, count);
+        	queue.remove();
+        	if(currWord.equals(endWord))
+        		return count; 
+        	for(int i=0;i<currWord.length();i++)
+        	{
+        		for(char c='a';c<='z';c++)
+        		{
+        			String newWord = currWord.replace(currWord.charAt(i),c);
+        			if(set.contains(newWord))
+        			{
+        				queue.add(new Pair(newWord,step+1));
+        				set.remove(newWord);
+        				break;
+        				
+        			}
+        		}
+        		
+        	}
+        	
+        }
+        
+        return 0;
+    }
+}
